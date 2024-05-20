96368d86bfc2b55d2144bf918eaf621186440a18
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-05-20 20:36:15 +0100

    menu

--- Assets/Scripts/Data/SongSelector.cs
+++ Assets/Scripts/Data/SongSelector.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SongSelector : MonoBehaviour {
 
     [SerializeField] GameObject _songButton;
-    [SerializeField] GameObject _levelsPanel;
+    [SerializeField] GameObject _levelsPanel, _songsPanel, _resumePanel, _songsButtons;
     SongLevel[] _levels;
 
     void Awake() {
@@ -30,15 +30,22 @@ public class SongSelector : MonoBehaviour {
         PlayerPrefs.SetInt("song.duration", song.duration);
         _levels = song.levels;
         _levelsPanel.SetActive(true);
+        _songsPanel.SetActive(false);
+        _songsButtons.SetActive(false);
     }
 
     public void SelectLevel(string level) {
-        foreach (SongLevel songLevel in _levels) 
+        foreach (SongLevel songLevel in _levels)
             if (songLevel.level == level) {
                 PlayerPrefs.SetString("song.level", level);
                 PlayerPrefs.SetString("song.level.path", songLevel.path);
                 PlayerPrefs.SetString("song.level.rankings", JsonUtility.ToJson(new LevelRankingArrayWrapper { levelRankingArray = songLevel.levelRanking.ToArray() }));
-                SCManager.instance.LoadScene("GameWip");
+                // SCManager.instance.LoadScene("GameWip");
+                _levelsPanel.SetActive(false);
+                _resumePanel.SetActive(true);
+
+                _resumePanel.transform.GetChild(0).GetComponent<TMP_Text>().text = $"Song: {$"Song: {PlayerPrefs.GetString("song.title")}"}";
+                _resumePanel.transform.GetChild(1).GetComponent<TMP_Text>().text = $"Level: {PlayerPrefs.GetString("song.level")}";
             }
     }
 
--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -3,17 +3,63 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     public GameObject CreditsPanel;
-   // public GameObject SettingsPanel;
+    public GameObject CreditsButtons;
+    public GameObject MenuButtons;
+    public GameObject SongsPanel;
+    public GameObject SongsButtons;
+    public GameObject LevelsButtons;
+    public GameObject ResumePanel;
+
+
 
     void Start() {
         AudioManager.instance.PlayMusic("menu");
     }
 
+    public void StartGame() => SCManager.instance.LoadScene("GameWip");
+
     public void QuitGame() => Application.Quit();
 
-    public void ShowCredits() => CreditsPanel.SetActive(true);
+    public void ShowCredits()
+    {
+        CreditsPanel.SetActive(true);
+        CreditsButtons.SetActive(true);
+        MenuButtons.SetActive(false);
+    }
 
-    public void HideCredits() => CreditsPanel.SetActive(false);
+    public void HideCredits()
+    {
+        CreditsPanel.SetActive(false);
+        CreditsButtons.SetActive(false);
+        MenuButtons.SetActive(true);
+    }
+
+    public void ShowSongs()
+    {
+        SongsPanel.SetActive(true);
+        SongsButtons.SetActive(true);
+        MenuButtons.SetActive(false);
+    }
+
+    public void HideSongs()
+    {
+        SongsPanel.SetActive(false);
+        SongsButtons.SetActive(false);
+        MenuButtons.SetActive(true);
+    }
+
+    public void ShowLevels()
+    {
+        LevelsButtons.SetActive(true);
+        MenuButtons.SetActive(false);
+    }
+
+    public void HideLevels()
+    {
+        ResumePanel.SetActive(false);
+        LevelsButtons.SetActive(false);
+        MenuButtons.SetActive(true);
+    }
     
     //public void ShowSettings() => SettingsPanel.SetActive(true);
 
