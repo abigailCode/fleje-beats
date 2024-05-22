9b54e3c519a3fad40438c373b211d6a2d2aa1e9a
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-05-22 20:30:29 +0100

    refactor: Remove unused Game Scene

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -16,7 +16,7 @@ public class Menu : MonoBehaviour
         AudioManager.instance.PlayMusic("menu");
     }
 
-    public void StartGame() => SCManager.instance.LoadScene("GameWip");
+    public void StartGame() => SCManager.instance.LoadScene("Game");
 
     public void QuitGame() => Application.Quit();
 
